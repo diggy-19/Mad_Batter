ffd99ed6a752c2acd880e059e6994e67101a5989
Dhruvi Vadalia <25223655+diggy-19@users.noreply.github.com>
2018-11-02 21:21:05 +0530

    Delete RecipePage_js.js

--- RecipePage_js.js
+++
@@ -1,7 +0,0 @@
-$('.rating-star').click(function() {
-    $(this).parents('.rating').find('.rating-star').removeClass('checked');
-    $(this).addClass('checked');
-
-    var submitStars = $(this).attr('data-value');
-    alert("You have given this recipe " + submitStars + " star(s)");
-});
